23156e897de68d1d9909d203406f2607d482a5ed
AlanYihengXu <alan.yiheng.xu@gmail.com>
2024-04-11 21:45:31 -0700

    Red-Black Tree WIP

--- ExtraCredit/SeeingForestForTrees/RedBlackTree.java
+++ ExtraCredit/SeeingForestForTrees/RedBlackTree.java
@@ -8,6 +8,7 @@ public class RedBlackTree<E extends Comparable<E>> {
         E val;
         Node left;
         Node right;
+        Node parent;
         boolean color; // red is true, black is false
 
         Node(E val, boolean color) {
@@ -22,6 +23,38 @@ public class RedBlackTree<E extends Comparable<E>> {
         root = null;
     }
 
+    private Node rotateLeft(Node n) {
+        Node parent = n.parent;
+        Node pivot = n.right;
+        n.right = pivot.left;
+        if (pivot.left != null) pivot.left.parent = n;
+        pivot.left = n;
+        n.parent = pivot;
+        if (parent == null) {
+            root = pivot;
+        } else {
+            if (n == parent.left) parent.left = pivot;
+            else parent.right = pivot;
+        }
+        return pivot;
+    }
+
+    private Node rotateRight(Node n) {
+        Node parent = n.parent;
+        Node pivot = n.left;
+        n.left = pivot.right;
+        if (pivot.right != null) pivot.right.parent = n;
+        pivot.right = n;
+        n.parent = pivot;
+        if (parent == null) {
+            root = pivot;
+        } else {
+            if (n == parent.left) parent.left = pivot;
+            else parent.right = pivot;
+        }
+        return pivot;
+    }
+
     public void insert(E element) { // Approach 1 for duplicates, add as right child
         if (isEmpty()) {
             root = new Node(element, false);
@@ -33,18 +66,60 @@ public class RedBlackTree<E extends Comparable<E>> {
             if (element.compareTo(current.val) < 0) {
                 if (current.left == null) {
                     current.left = new Node(element, true);
-                    // REBALANCE
+                    current.left.parent = current;
+                    rebalanceInsert(current.left);
                     return;
                 }
                 current = current.left;
             } else {
                 if (current.right == null) {
                     current.right = new Node(element, true);
-                    // REBALANCE
+                    current.right.parent = current;
+                    rebalanceInsert(current.right);
                     return;
                 }
                 current = current.right;
             }
+        }
+    }
+    
+    private void rebalanceInsert(Node current) {
+        if (!current.parent.color) return;
+        Node grandparent = current.parent.parent;
+        Node uncle = (current.parent == grandparent.left) ? grandparent.right : grandparent.left;
+
+        if (uncle != null && uncle.color) {
+            uncle.color = false;
+            current.parent.color = false;
+            if (grandparent != root) {
+                grandparent.color = true;
+                rebalanceInsert(grandparent);
+            }
+            return;
+        }
+
+        if (grandparent.left != null && current == grandparent.left.left) { // Case 1
+            rotateRight(grandparent);
+            boolean temp = grandparent.color;
+            grandparent.color = current.parent.color;
+            current.parent.color = temp;
+        } else if (grandparent.left != null && current == grandparent.left.right) { // Case 2
+            rotateLeft(current.parent);
+            rotateRight(grandparent);
+            boolean temp = grandparent.color;
+            grandparent.color = current.color;
+            current.color = temp;
+        } else if (grandparent.right != null && current == grandparent.right.right) { // Case 3
+            rotateLeft(grandparent);
+            boolean temp = grandparent.color;
+            grandparent.color = current.parent.color;
+            current.parent.color = temp;
+        } else { // Case 4
+            rotateRight(current.parent);
+            rotateLeft(grandparent);
+            boolean temp = grandparent.color;
+            grandparent.color = current.color;
+            current.color = temp;
         }
     }
 
@@ -52,89 +127,72 @@ public class RedBlackTree<E extends Comparable<E>> {
         if (isEmpty()) {
             return false;
         }
-
-        Node deleted, successorParent;
         Node current = root;
-
-        if (current.val == element) {
-            if (current.right == null) {
-                root = current.left;
-                //REBALANCE
-                return true;
-            }
-            successorParent = findSuccessorParent(current);
-            if (successorParent == current) {
-                root = current.right;
-                //REBALANCE
-                return true;
-            }
-            root = successorParent.left;
-            successorParent.left = null;
-            root.left = current.left;
-            root.right = current.right;
-            //REBALANCE
-            return true;
-        }
 
         while (current != null) {
             if (element.compareTo(current.val) < 0) {
-                if (current.left == null) return false;
-                if (current.left.val == element) {
-                    deleted = current.left;
-                    if (deleted.right == null) {
-                        current.left = deleted.left;
-                        //REBALANCE
-                        return true;
-                    }
-                    successorParent = findSuccessorParent(deleted);
-                    if (successorParent == deleted) {
-                        current.left = deleted.right;
-                        //REBALANCE
-                        return true;
-                    }
-                    current.left = successorParent.left;
-                    successorParent.left = null;
-                    current.left.left = deleted.left;
-                    current.left.right = deleted.right;
-                    //REBALANCE
-                    return true;
-                }
                 current = current.left;
-            } else {
-                if (current.right == null) return false;
-                if (current.right.val == element) {
-                    deleted = current.right;
-                    if (deleted.right == null) {
-                        current.right = deleted.left;
-                        //REBALANCE
-                        return true;
-                    }
-                    successorParent = findSuccessorParent(deleted);
-                    if (successorParent == deleted) {
-                        current.right = deleted.right;
-                        //REBALANCE
-                        return true;
-                    }
-                    current.right = successorParent.left;
-                    successorParent.left = null;
-                    current.right.left = deleted.left;
-                    current.right.right = deleted.right;
-                    //REBALANCE
-                    return true;
-                }
+            } else if (element.compareTo(current.val) > 0) {
                 current = current.right;
+            } else {
+                // Delete node if less than 2 children
+                if (lessThan2Nodes(current)) return true;
+                
+                // Swap value node value with successor
+                Node successor = current.right;
+                while(successor.left != null) {
+                    successor = successor.left;
+                }
+                current.val = successor.val;
+
+                // Deleted successor node
+                return lessThan2Nodes(successor);
             }
         }
         return false;
     }
 
-    private Node findSuccessorParent(Node current) {
-        if (current.right.left == null) return current;
-        current = current.right;
-        while(current.left.left != null) {
-            current = current.left;
+    private boolean lessThan2Nodes(Node current) {
+        if (current.left == null && current.right == null) {// Leaf node
+            if (current == root) {
+                root = null;
+            } else if (current.color) { // Node is red
+                if (current == current.parent.left) current.parent.left = null;
+                else current.parent.right = null;
+            } else { // Non-root black node
+
+            }
+            return true;
         }
-        return current;
+
+        if (current.right == null) { // Only left child
+            if (current == root) {
+                root = current.left;
+            } else {
+                if (current == current.parent.left) current.parent.left = current.left;
+                else current.parent.right = current.left;
+            }
+            if (current.left != null) {
+                current.left.parent = current.parent;
+                current.left.color = false; //Recolor black
+            }   
+            return true;
+        }
+
+        if (current.left == null) { // Only right child
+            if (current == root) {
+                root = current.right;
+            } else {
+                if (current == current.parent.left) current.parent.left = current.right;
+                else current.parent.right = current.right;
+            }
+            if (current.right != null) {
+                current.right.parent = current.parent;
+                current.right.color = false; //Recolor black
+            }   
+            return true;
+        }
+        return false;
     }
 
     public boolean contains(E element) {
---
+++ Labs/Lab6/Lab6.java
@@ -0,0 +1,8 @@
+package Labs.Lab6;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class Lab6 {
+    
+}
